6a2826721929c01d45da3587445da5c331f58b53
Andrei Martins <92amartins@gmail.com>
2020-09-21 15:58:17 -0300

    Document and export hint()

--- R/hint.R
+++ R/hint.R
@@ -1,3 +1,15 @@
+#' Hint action for a stock
+#'
+#' Suggests a action (buy, hold or sell) for a specific stock.
+#'
+#' @param stock_code character
+#'
+#' @return character
+#'
+#' @export
+#'
+#' @examples
+#' hint("VALE3.SA")
 hint <- function(stock_code) {
   sample(c("buy", "hold", "sell"), 1)
 }

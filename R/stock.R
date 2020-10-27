2740aca1ba4af3254a03c74aca09cc6ef8697a19
Andrei Martins <92amartins@gmail.com>
2020-10-27 16:04:48 -0300

    Add monthly variation function

--- R/stock.R
+++ R/stock.R
@@ -20,3 +20,30 @@ stock <- function(stock_code) {
 
   stock_data
 }
+
+#' Return monthly variation of a asset
+#'
+#' Given a `stock_data` data.frame, increment data with a `Variation`.
+#'
+#' @param stock_data data.frame
+#'
+#' @import dplyr
+#'
+#' @return data.frame
+#'
+#' @export
+#'
+#' @examples
+#' raw_stock = stock("VALE3.SA")
+#' add_monthly_variation(raw_stock, '2019-10-26')
+add_monthly_variation <- function(stock_data, start_date) {
+  stock_data %>%
+    mutate(Month = lubridate::floor_date(as.Date(Date), "month")) %>%
+    group_by(Month) %>%
+    mutate(rank = rank(Date)) %>%
+    filter(rank == 1) %>%
+    ungroup(Month) %>%
+    mutate(Variation = (Close - lag(Close)) / lag(Close)) %>%
+    filter(Date >= start_date) %>%
+    select(Month, Close, Variation)
+}
--- tests/testthat/test-stock.R
+++ tests/testthat/test-stock.R
@@ -8,3 +8,15 @@ test_that("stock() returns historical price data", {
   expect(nrow(vale3) > 0, "stock() unexpectedly returned 0 rows.")
   expect_equal(names(vale3), expected_columns)
 })
+
+test_that("add_monthly_variation() adds variation column", {
+  stock_code <- c("VALE3.SA")
+  expected_columns <- c("Month", "Close", "Variation")
+
+  raw_vale3 <- stock(stock_code)
+  vale3 <- add_monthly_variation(raw_vale3, "2019-10-26")
+
+
+  expect(nrow(vale3) > 0, "add_monthly() unexpectedly returned 0 rows.")
+  expect_equal(names(vale3), expected_columns)
+})

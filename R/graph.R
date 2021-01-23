283e7aa3517024b3f5d5bf5bf882c5f5b966c9d9
Andrei Martins <92amartins@gmail.com>
2021-01-23 16:41:55 -0300

    Improve graphs appearance

--- R/graph.R
+++ R/graph.R
@@ -1,3 +1,6 @@
+#' GLobally set a chart theme
+ggplot2::theme_set(hrbrthemes::theme_ft_rc())
+
 #' Display a graph of an asset variation over months
 #'
 #' @param stock_code character
@@ -7,20 +10,27 @@
 #'
 #' @import ggplot2
 #' @import scales
+#' @import hrbrthemes
 #'
 #' @export
 #'
 #' @examples
 #' graph("VALE3.SA", "2019-10-31")
 graph <- function(stock_code, start_date) {
-  asset <- stock(stock_code) %>% add_monthly_variation(start_date) %>% mutate(Date = as.Date(Month))
+  asset <- stock(stock_code) %>%
+      add_monthly_variation(start_date) %>%
+      mutate(Date = as.Date(Month)) %>%
+      mutate(Sign = as.character(sign(Variation)))
 
-  ggplot(asset, aes(Month, Variation)) +
+  ggplot(asset, aes(Month, Variation, color=Sign, fill=Sign)) +
     geom_bar(position=position_dodge(), stat='identity') +
-    geom_text(aes(label = sprintf("%0.2f", round(Variation, digits = 2))), size = 4, vjust = 1.2) +
+    geom_text(aes(label = sprintf("%0.2f", round(Variation, digits = 2))), color = "white", size = 4, vjust = 1.2) +
     labs(
-      title = paste(stock_code, "variation")
+      title = stock_code,
+      subtitle = "Monthly Variation",
+      caption = "Source: Yahoo Finance (powered by quantmod)."
     ) +
+    theme(legend.position = "none") +
     scale_x_date(labels = date_format("%b-%Y"), date_breaks = "1 month")
 }
 
@@ -39,12 +49,19 @@ graph <- function(stock_code, start_date) {
 #' @examples
 #' weekly_graph("VALE3.SA", "2019-10-31")
 weekly_graph <- function(stock_code, start_date) {
-  asset <- stock(stock_code) %>% add_weekly_variation(start_date)
+  asset <- stock(stock_code) %>%
+    add_weekly_variation(start_date) %>%
+    mutate(Week = as.Date(Date)) %>%
+    mutate(Sign = as.character(sign(Variation)))
 
-  ggplot(asset, aes(Date, Variation)) +
+  ggplot(asset, aes(Week, Variation, color=Sign, fill=Sign)) +
     geom_bar(position=position_dodge(), stat='identity') +
-    geom_text(aes(label = sprintf("%0.2f", round(Variation, digits = 2))), size = 4, vjust = 1.2) +
+    geom_text(aes(label = sprintf("%0.2f", round(Variation, digits = 2))), color="white", size = 4, vjust = 1.2) +
     labs(
-      title = paste(stock_code, "weekly variation")
-    )
+      title = stock_code,
+      subtitle = "Weekly Variation",
+      caption = "Source: Yahoo Finance (powered by quantmod)."
+    ) +
+    theme(legend.position = "none") +
+    scale_x_date(labels = date_format("%U-%Y"), date_breaks = "7 days")
 }
--- tests/testthat/test-graph.R
+++ tests/testthat/test-graph.R
@@ -3,7 +3,9 @@ test_that("graph() returns a graph", {
   g <- graph(stock_code, "2020-01-01")
 
   expect(nrow(g$data) > 0, "no data available. check stock() call.")
-  expect(g$labels$title == "PETR4.SA variation", "malformed title.")
+  expect(g$labels$title == "PETR4.SA", "malformed title.")
+  expect(g$labels$subtitle == "Monthly Variation", "malformed subtitle.")
+  expect(g$labels$caption == "Source: Yahoo Finance (powered by quantmod).", "malformed caption.")
   expect(g$labels$x == "Month", "malformed x-axis.")
   expect(g$labels$y == "Variation", "malformed y-axis")
 })
@@ -13,7 +15,9 @@ test_that("weekly_graph() returns a weekly graph", {
   g <- weekly_graph(stock_code, "2020-01-01")
 
   expect(nrow(g$data) > 0, "no data available. check stock() call.")
-  expect(g$labels$title == "PETR4.SA weekly variation", "malformed title.")
-  expect(g$labels$x == "Date", "malformed x-axis.")
+  expect(g$labels$title == "PETR4.SA", "malformed title.")
+  expect(g$labels$subtitle == "Weekly Variation", "malformed subtitle.")
+  expect(g$labels$caption == "Source: Yahoo Finance (powered by quantmod).", "malformed caption.")
+  expect(g$labels$x == "Week", "malformed x-axis.")
   expect(g$labels$y == "Variation", "malformed y-axis")
 })
